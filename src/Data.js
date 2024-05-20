a9f1bb9452ac64c6f3d4c3f120d78ffd4d1001fd
k0wal45 <d.kowal.com@gmail.com>
2024-05-20 13:42:10 +0200

    photo change

--- src/Data.js
+++ src/Data.js
@@ -3,6 +3,14 @@ import mikolajowe1 from './assets/img/mikolajoweRozdanie/mikolajowe1.webp'
 import mikolajowe2 from './assets/img/mikolajoweRozdanie/mikolajowe2.webp'
 import mikolajowe3 from './assets/img/mikolajoweRozdanie/mikolajowe3.webp'
 import mikolajowe4 from './assets/img/mikolajoweRozdanie/mikolajowe4.webp'
+import mikolajowe5 from './assets/img/mikolajoweRozdanie/mikolajowe5.webp'
+import mikolajowe6 from './assets/img/mikolajoweRozdanie/mikolajowe6.webp'
+import mikolajowe7 from './assets/img/mikolajoweRozdanie/mikolajowe7.webp'
+import mikolajowe8 from './assets/img/mikolajoweRozdanie/mikolajowe8.webp'
+import mikolajowe9 from './assets/img/mikolajoweRozdanie/mikolajowe9.webp'
+import mikolajowe10 from './assets/img/mikolajoweRozdanie/mikolajowe10.webp'
+import mikolajowe11 from './assets/img/mikolajoweRozdanie/mikolajowe11.webp'
+import mikolajowe12 from './assets/img/mikolajoweRozdanie/mikolajowe12.webp'
 
 // xxxxx
 
@@ -10,7 +18,7 @@ const Data = [
   {
     title: 'Wielkie Mikołajkowe Rozdawanie Prezentów',
     link: 'wielkie-mikolajkowe-rozdawanie-prezentow',
-    photo: [mikolajowe4, mikolajowe2, mikolajowe3, mikolajowe1],
+    photo: [mikolajowe4, mikolajowe2, mikolajowe3, mikolajowe1, mikolajowe5, mikolajowe6, mikolajowe7, mikolajowe8, mikolajowe9, mikolajowe10, mikolajowe11, mikolajowe12],
     description: 'W tegoroczną magiczną porę świąteczną dzieci z naszej społeczności cieszyły się szczególnym zaskoczeniem i radością, gdy Mikołaj przyniósł im prezenty od dwóch ważnych podmiotów: Fundacji oraz fundatora Medica Przychodnia. Wspólna inicjatywa obu organizacji sprawiła, że dla najmłodszych tegoroczne święta stały się niezapomniane. Fundacja, z misją poprawy warunków życia dzieci, dostarczyła nie tylko upominki, ale także przekazała ciepło i wsparcie. Wspólnie z fundatorem Merkury, który zawsze troszczy się o zdrowie społeczności lokalnej, stworzyliśmy atmosferę, w której magia świąt przeniknęła serca najmłodszych.',
     shortDescription: 'Dzieci w tym roku z uśmiechem na twarzach przyjęły prezenty od Fundacji oraz fundatora Merkury Medica',
     date: new Date('2023-12-06'),
